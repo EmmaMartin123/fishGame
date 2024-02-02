e203c337b71f04a8f181d32bf09c9a24af7b01ec
EmmaMartin123 <114400463+EmmaMartin123@users.noreply.github.com>
2024-02-02 15:01:51 +0000

    Add files via upload

---
+++ fishGame/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace fishGame
+{
+    partial class frmGameScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rulesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblRedLabel = new System.Windows.Forms.Label();
+            this.lblBlueLabel = new System.Windows.Forms.Label();
+            this.lblRedPoints = new System.Windows.Forms.Label();
+            this.lblBluePoints = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(853, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.rulesToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // rulesToolStripMenuItem
+            // 
+            this.rulesToolStripMenuItem.Name = "rulesToolStripMenuItem";
+            this.rulesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.rulesToolStripMenuItem.Text = "Rules";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lblRedLabel
+            // 
+            this.lblRedLabel.AutoSize = true;
+            this.lblRedLabel.Location = new System.Drawing.Point(12, 38);
+            this.lblRedLabel.Name = "lblRedLabel";
+            this.lblRedLabel.Size = new System.Drawing.Size(45, 20);
+            this.lblRedLabel.TabIndex = 1;
+            this.lblRedLabel.Text = "Red:";
+            // 
+            // lblBlueLabel
+            // 
+            this.lblBlueLabel.AutoSize = true;
+            this.lblBlueLabel.Location = new System.Drawing.Point(12, 58);
+            this.lblBlueLabel.Name = "lblBlueLabel";
+            this.lblBlueLabel.Size = new System.Drawing.Size(46, 20);
+            this.lblBlueLabel.TabIndex = 2;
+            this.lblBlueLabel.Text = "Blue:";
+            // 
+            // lblRedPoints
+            // 
+            this.lblRedPoints.AutoSize = true;
+            this.lblRedPoints.Location = new System.Drawing.Point(63, 38);
+            this.lblRedPoints.Name = "lblRedPoints";
+            this.lblRedPoints.Size = new System.Drawing.Size(18, 20);
+            this.lblRedPoints.TabIndex = 3;
+            this.lblRedPoints.Text = "0";
+            // 
+            // lblBluePoints
+            // 
+            this.lblBluePoints.AutoSize = true;
+            this.lblBluePoints.Location = new System.Drawing.Point(63, 58);
+            this.lblBluePoints.Name = "lblBluePoints";
+            this.lblBluePoints.Size = new System.Drawing.Size(14, 16);
+            this.lblBluePoints.TabIndex = 4;
+            this.lblBluePoints.Text = "0";
+            // 
+            // frmGameScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 646);
+            this.Controls.Add(this.lblBluePoints);
+            this.Controls.Add(this.lblRedPoints);
+            this.Controls.Add(this.lblBlueLabel);
+            this.Controls.Add(this.lblRedLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmGameScreen";
+            this.Text = "gameScreen";
+            this.Load += new System.EventHandler(this.frmGameScreen_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rulesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lblRedLabel;
+        private System.Windows.Forms.Label lblBlueLabel;
+        private System.Windows.Forms.Label lblRedPoints;
+        private System.Windows.Forms.Label lblBluePoints;
+    }
+}
+
---
+++ fishGame/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fishGame
+{
+    public partial class frmGameScreen : Form
+    {
+        Button[,] btn = new Button[10, 10];
+        Random rnd = new Random();
+        int num = 0;
+        bool redPlaying = false;
+        bool bluePlaying = false;
+        int redPoints = 0;
+        int bluePoints = 0;
+        bool firstTurns = false;
+        string prevTile = "-1,-1";
+        int prevX = -1;
+        int prevY = -1;
+        int currentX = -1;
+        int currentY = -1;
+        bool obstruction = false;
+
+        public frmGameScreen()
+        {
+            InitializeComponent();
+            for (int i = 0; i < btn.GetLength(0); i++) 
+            {
+                for (int j = 0; j < btn.GetLength(1); j++)
+                {
+                    btn[i, j] = new Button();
+                    btn[i, j].SetBounds(100 + (45 * i), 25 + (45 * j), 45, 45);
+                    btn[i, j].BackColor = Color.PowderBlue;
+                    btn[i, j].Text = Convert.ToString(num = rnd.Next(1, 4));
+                    btn[i, j].Name = Convert.ToString(i + "," + j);
+                    btn[i, j].Click += new EventHandler(this.btnEvent_Click);
+                    Controls.Add(btn[i, j]);
+                }
+            }
+
+            DialogResult result;
+            result = MessageBox.Show("Red plays first! Click a box to make your first move.", "Red First Move", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            firstTurns = true;
+            redPlaying = true;
+        }
+        void btnEvent_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine(((Button)sender).Name);
+
+
+
+
+
+            if (redPlaying == false && bluePlaying == false)
+            {
+                if (((Button)sender).BackColor == Color.Red)
+                {
+                    redPlaying = true;
+                    ((Button)sender).BackColor = Color.Black;
+                    ((Button)sender).Text = "0";
+                    prevTile = ((Button)sender).Name;
+                }
+                else if (((Button)sender).BackColor == Color.Blue)
+                {
+                    bluePlaying = true;
+                    ((Button)sender).BackColor = Color.Black;
+                    ((Button)sender).Text = "0";
+                    prevTile = ((Button)sender).Name;
+                }
+            }
+            else if (redPlaying == true) //blue false?
+            {
+                string[] prev = prevTile.Split(',');
+                prevX = Int32.Parse(prev[0]);
+                prevY = Int32.Parse(prev[1]);
+
+                string[] current = ((Button)sender).Name.Split(',');
+                currentX = Int32.Parse(current[0]);
+                currentY = Int32.Parse(current[1]);
+
+                if ((currentX == prevX || currentY == prevY) || firstTurns == true)
+                {
+                    if (currentX == prevX)
+                    {
+                        for (int i = 1; i< Math.Abs(prevY - currentY); i++)
+                        {
+                            if (prevY < currentY)
+                            {
+                                if (btn[currentX, (i + prevY)].BackColor != Color.PowderBlue)
+                                    obstruction = true;
+                            }
+
+                            else if (prevY > currentY)
+                            {
+                                if (btn[currentX, (i + currentY)].BackColor != Color.PowderBlue)
+                                    obstruction = true;
+                            }
+                        }
+                    }
+
+                    if (currentY == prevY)
+                    {
+                        for (int i = 1; i < Math.Abs(prevX - currentX); i++)
+                        {
+                            if (prevX < currentX)
+                            {
+                                if (btn[(i + prevX), currentY].BackColor != Color.PowderBlue)
+                                    obstruction = true;
+                            }
+
+                            else if (prevX > currentX)
+                            {
+                                if (btn[(i + currentX), currentY].BackColor != Color.PowderBlue)
+                                    obstruction = true;
+                            }
+                        }
+                    }
+
+                    if ((((Button)sender).BackColor == Color.PowderBlue) && obstruction == false)
+                    {
+                        ((Button)sender).BackColor = Color.Red;
+                        num = Int32.Parse(((Button)sender).Text);
+                        redPoints += num;
+                        ((Label)lblRedPoints).Text = redPoints.ToString();
+                        redPlaying = false;
+
+                        if (firstTurns == true)
+                            bluePlaying = true;
+                    }
+                }
+            }
+            else if (bluePlaying == true)
+            {
+                string[] prev = prevTile.Split(',');
+                prevX = Int32.Parse(prev[0]);
+                prevY = Int32.Parse(prev[1]);
+
+                string[] current = ((Button)sender).Name.Split(',');
+                currentX = Int32.Parse(current[0]);
+                currentY = Int32.Parse(current[1]);
+
+                if ((currentX == prevX || currentY == prevY) || firstTurns == true)
+                {
+                    if (currentX == prevX)
+                    {
+                        for (int i = 1; i < Math.Abs(prevY - currentY); i++)
+                        {
+                            if (prevY < currentY)
+                            {
+                                if (btn[currentX, (i + prevY)].BackColor != Color.PowderBlue)
+                                    obstruction = true;
+                            }
+
+                            else if (prevY > currentY)
+                            {
+                                if (btn[currentX, (i + currentY)].BackColor != Color.PowderBlue)
+                                    obstruction = true;
+                            }
+                        }
+                    }
+
+                    if (currentY == prevY)
+                    {
+                        for (int i = 1; i < Math.Abs(prevX - currentX); i++)
+                        {
+                            if (prevX < currentX)
+                            {
+                                if (btn[(i + prevX), currentY].BackColor != Color.PowderBlue)
+                                    obstruction = true;
+                            }
+
+                            else if (prevX > currentX)
+                            {
+                                if (btn[(i + currentX), currentY].BackColor != Color.PowderBlue)
+                                    obstruction = true;
+                            }
+                        }
+                    }
+
+                    if ((((Button)sender).BackColor == Color.PowderBlue) && obstruction == false)
+                    {
+                        ((Button)sender).BackColor = Color.Blue;
+                        num = Int32.Parse(((Button)sender).Text);
+                        bluePoints += num;
+                        ((Label)lblBluePoints).Text = bluePoints.ToString();
+                        bluePlaying = false;
+
+                        if (firstTurns == true)
+                            firstTurns = false;
+                    }
+                }
+            }
+
+            obstruction = false;
+        }
+
+            private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show("Penguin Game in C# by Emma Martin, Martyn Bett and Lucy Thomson (c) 2024", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void frmGameScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
